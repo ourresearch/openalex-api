8f4c41837a38a9240ae84be3ffa7646eb8de543b
Heather Piwowar <hpiwowar@gmail.com>
2019-05-06 15:43:09 -0700

    update save rows

--- save_issn_api.py
+++ save_issn_api.py
@@ -6,12 +6,27 @@ from time import time
 from time import sleep
 import datetime
 from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy import sql
 from sqlalchemy import orm
 
 from app import db
 from util import elapsed
 from util import safe_commit
+
+class JournalMetadataRaw(db.Model):
+    issnl = db.Column(db.Text, primary_key=True)
+    journal_title = db.Column(db.Text)
+    publisher = db.Column(db.Text)
+    issns = db.Column(ARRAY(db.Text), server_default='[]')
+    api_raw_issn = db.Column(JSONB)
+    api_raw_crossref = db.Column(JSONB)
+
+    def __repr__(self):
+        return u'<JournalMetadataRaw ({issnl})>'.format(
+            issnl=self.issnl
+        )
+
 
 def call_issn_api(query_text):
     if not query_text:
@@ -35,28 +50,26 @@ def call_issn_api(query_text):
     # print response_data
     return response_data
 
+def call_crossref_api(query_text):
+    if not query_text:
+        return None
+
+    response_data = None
+
+    url_template = u"https://api.crossref.org/journals/{}"
+    url = url_template.format(query_text)
+    print url
+    r = requests.get(url)
+    if r.status_code == 200:
+        try:
+            response_data = r.json()
+        except ValueError:
+            pass
+
+    # print response_data
+    return response_data
 
 
-def store_issn_api(my_queue_save_obj):
-    result = call_issn_api(my_queue_save_obj.issnl)
-    if not result:
-        result = "ERROR"
-    my_queue_save_obj.api_raw = result
-    db.session.merge(my_queue_save_obj)
-    safe_commit(db)
-    print ".",
-    sleep(3)
-
-
-class QueueSave(db.Model):
-    __tablename__ = "journal_issnl_api"
-    issnl = db.Column(db.Text, primary_key=True)
-    api_raw = db.Column(JSONB)
-
-    def __repr__(self):
-        return u'<QueueSave ({issnl})>'.format(
-            issnl=self.issnl
-        )
 
 
 
@@ -67,18 +80,36 @@ if __name__ == "__main__":
 
     start = time()
 
-    if __name__ == '__main__':
-        for i in range(100000):
-            query = QueueSave.query.filter(QueueSave.api_raw == None)\
-                .order_by(QueueSave.issnl.desc())\
-                .limit(1)
-            queue_save_objs = query.all()
+    while True:
+        query = JournalMetadataRaw.query.filter(JournalMetadataRaw.api_raw_crossref == None)\
+            .order_by(JournalMetadataRaw.issnl.desc())\
+            .limit(10)
+        journal_metadata_objs = query.all()
 
-            for save_obj in queue_save_objs:
-                print save_obj
-                store_issn_api(save_obj)
+        if not journal_metadata_objs:
+            print "done!"
+            exit()
 
-            db.session.remove()
-            # print "finished update in {}sec".format(elapsed(start))
-            print "*",
+        for my_obj in journal_metadata_objs:
+            # if not my_obj.api_raw_issn:
+            #     result = call_issn_api(my_obj.issnl)
+            #     if not result:
+            #         result = "ERROR"
+            #     my_obj.api_raw_issn = result
+            #     sleep(3)
+
+            if not my_obj.api_raw_crossref:
+                result = call_crossref_api(my_obj.issnl)
+                if not result:
+                    result = "ERROR"
+                my_obj.api_raw_crossref = result
+
+            db.session.merge(my_obj)
+
+        safe_commit(db)
+        print ".",
+
+        db.session.remove()
+        # print "finished update in {}sec".format(elapsed(start))
+        print "*",
 
