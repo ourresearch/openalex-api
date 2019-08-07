d2fb8df27cca34fa87380368d0200c386c2eda3c
Heather Piwowar <hpiwowar@gmail.com>
2019-08-07 06:39:42 -0700

    faster text queries

--- views.py
+++ views.py
@@ -515,22 +515,40 @@ def unpaywall_journals_articles_paged():
 
     offset = (page - 1) * pagesize
 
+    # command = """
+    #         select api_json
+    #         from unpaywall_production j
+    #         join cdl_journals_temp_with_issn_l_dist_all cdl on j.journal_issn_l = cdl.issn_l
+    #         where
+    #         (j.published_date > coalesce(cdl.from_date, '1900-01-01'::timestamp) and j.published_date < coalesce(cdl.to_date, '2100-01-01'::timestamp))
+    #         {text_filter}
+    #         {oa_filter}
+    #         order by published_date desc
+    #         limit {pagesize}
+    #         offset {offset}
+    #     """.format(pagesize=pagesize,
+    #                offset=offset,
+    #                text_filter=build_text_filter(),
+    #                oa_filter=build_oa_filter())
+
     command = """
-            select api_json
+        select u.doi, api_json 
+        from unpaywall_simple_sortkey u, 
+        (   select doi
             from unpaywall_production j
             join cdl_journals_temp_with_issn_l_dist_all cdl on j.journal_issn_l = cdl.issn_l
             where 
-            j.published_date > coalesce(cdl.from_date, '1900-01-01'::timestamp) and j.published_date < coalesce(cdl.to_date, '2100-01-01'::timestamp) 
-            {text_filter}
-            {oa_filter}
-            order by published_date desc 
+                j.published_date > coalesce(cdl.from_date, '1900-01-01'::timestamp) and j.published_date < coalesce(cdl.to_date, '2100-01-01'::timestamp) 
+                {text_filter}
+                {oa_filter}
+            order by published_date desc
             limit {pagesize}
-            offset {offset}
-        """.format(pagesize=pagesize,
+            offset {offset}) as s
+        where u.doi=s.doi    
+    """.format(pagesize=pagesize,
                    offset=offset,
                    text_filter=build_text_filter(),
                    oa_filter=build_oa_filter())
-
     print command
     with get_db_cursor() as cursor:
         cursor.execute(command)
